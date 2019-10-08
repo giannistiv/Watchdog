320135e12ee64c14f6cf2aae3b209a4153f77f1e
johnstiv <stivaktakis21@gmail.com>
2019-10-08 15:49:25 +0300

    hog algorithm

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,7 +1,25 @@
-import os
-import io
-import Image
-from array import array
+#import os
+#import io
+#import Image
+#from array import array
+#import cv2
+#import numpy as np
 
-image = Image.open(io.BytesIO(bytes))
-image.save(savepath)
+def imageproc(arr):
+    frame = Image.open(io.BytesIO(arr))
+
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    frame = cv2.resize(frame, (600, 400))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8)
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+    for (xA, yA, xB, yB) in boxes:
+    # display the detected boxes in the colour picture
+    cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+                                    
+    
